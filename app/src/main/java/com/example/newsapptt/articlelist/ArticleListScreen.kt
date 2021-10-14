1d09faea7b96861eee9f5e8945f0e94f9abe7a99
Musadrammeh <43375006+Musadrammeh@users.noreply.github.com>
2021-10-14 14:10:00 +0200

    feat: frontpage_searchbarAndLogo

--- app/src/main/java/com/example/newsapptt/articlelist/ArticleListScreen.kt
+++ app/src/main/java/com/example/newsapptt/articlelist/ArticleListScreen.kt
@@ -1,18 +1,26 @@
 package com.example.newsapptt.articlelist
 
+import android.app.Activity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.leanback.widget.SearchBar
 import androidx.navigation.NavController
 import com.example.newsapptt.R
 import com.google.firebase.database.collection.LLRBNode
@@ -35,6 +43,17 @@ fun ArticleListScreen(
                     .fillMaxWidth()
                     .align(CenterHorizontally)
             )
+            searchBar(
+                hint = "Search article...",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ){
+
+            }
+
+
+
 
         }
     }
@@ -60,7 +79,24 @@ fun searchBar(
             },
             maxLines = 1,
             singleLine = true,
-            textStyle = TextStyle(color = Color.Black)
+            textStyle = androidx.compose.ui.text.TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    ishintDisplayed
+                }
         )
+        if(ishintDisplayed){
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+
+            )
+        }
     }
 }
